4cc98b362b76ca44bf7ff1fff032442a7bd40a19
WIRTEK\vlad.teodorescu <vladjohn2013@gmail.com>
2017-07-25 16:52:54 +0300

    MOre pages

--- app/Components/AppViewer.tsx
+++ app/Components/AppViewer.tsx
@@ -1,16 +1,13 @@
 import * as React from "react"
-import {Category} from "Components/Elements/Category"
-import {ListView} from "Components/Elements/ListView"
-import {UserList} from "Components/Elements/UserList"
+import {MainPage} from "Components/Pages/MainPage"
+
 
 export class AppViewer extends React.Component <{},{}>
 {
     render(){
         return (
             <div className = "AppViewer">
-                <ListView elements={[<Category imgurl="interiordesign.jpg" name="Interior design" description="Get help with designing your home"/> ,
-                <Category imgurl="law.jpg" name="Law" description="Get help from a lawyer"/>,
-                <UserList imgurl="profile_img.png" name="Bob" bio="CEO at some corporation" rating={4}/> ]}/>
+               <MainPage/>
             </div>
         );
     }
