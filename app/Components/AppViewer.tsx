ed1f16e5cd13bf1908949979e17ad77e69a0c48b
WIRTEK\vlad.teodorescu <vladjohn2013@gmail.com>
2017-07-25 12:45:32 +0300

    Sign Up Page (probably initial version and not fully correct)

--- app/Components/AppViewer.tsx
+++ app/Components/AppViewer.tsx
@@ -1,11 +1,14 @@
 import * as React from "react"
+import {SignUp} from "Components/Pages/SignUpPage"
 
 export class AppViewer extends React.Component <{},{}>
 {
     render(){
         return (
             <div className = "AppViewer">
-                <p>This is a body.</p>
+                <p>This is a body.
+                    <SignUp/>
+                </p>
             </div>
         );
     }
---
+++ app/Components/Pages/SignUpPage.tsx
@@ -0,0 +1,61 @@
+import * as React from "react"
+import {Button, Form, FormControl, FormGroup, Col, ControlLabel} from "react-bootstrap"
+
+export class SignUp extends React.Component <{},{}>
+{
+    render(){
+        return(
+            <div className='SignUp'>
+                <div style={{backgroundColor: '#8babe0'}}>
+                    <Form horizontal style={{padding:'20px'}}>
+                        <FormGroup controlId='SignUpName'>
+                            <Col componentClass={ControlLabel} sm={2}>
+                                Name
+                            </Col>
+                            <Col sm={5}>
+                                <FormControl type='text' placeholder='Your full name'/>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup controlId='SignUpUsername'>
+                            <Col componentClass={ControlLabel} sm={2}>
+                                Username
+                            </Col>
+                            <Col sm={5}>
+                                <FormControl type='text' placeholder='Your username'/>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup controlId='SignUpPassword'>
+                            <Col componentClass={ControlLabel} sm={2}>
+                                Password
+                            </Col>
+                            <Col sm={5}>
+                                <FormControl type='password' placeholder='Your password'/>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup controlId='SignUpConfirmPassword'>
+                            <Col componentClass={ControlLabel} sm={2}>
+                                Confirm Password
+                            </Col>
+                            <Col sm={5}>
+                                <FormControl type='password' placeholder='Confirm your password'/>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup controlId='SignUpEmail'>
+                            <Col componentClass={ControlLabel} sm={2}>
+                                Email
+                            </Col>
+                            <Col sm={5}>
+                                <FormControl type='email' placeholder='Your email'/>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup controlId='SignUpSubmit'>
+                            <Col sm={7} smOffset={4}>
+                                <Button type='submit' bsStyle='primary'>Sign Up</Button>
+                            </Col>
+                        </FormGroup>
+                    </Form>
+                </div>  
+            </div>
+        );
+    }
+}
