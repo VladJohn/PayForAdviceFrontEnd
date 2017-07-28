7e0108655aa57eef50a8b1cf5bc2cbf48e421cbe
WIRTEK\vlad.teodorescu <vladjohn2013@gmail.com>
2017-07-28 10:10:17 +0300

    POst SignUp si LogIn

--- app/Components/Pages/SignUpPage.tsx
+++ app/Components/Pages/SignUpPage.tsx
@@ -2,6 +2,9 @@ import * as React from "react"
 
 export class SignUpPage extends React.Component <{},{name : string, username:string, password:string, cfpassword:string, email:string}>
 {
+    baseUrl: string = 'http://localhost:52619/api/user/';
+    headers: Headers;
+
     constructor() {
     super();
     this.state = {name:'', username:'', password:'', cfpassword:'', email:''};
@@ -11,6 +14,24 @@ export class SignUpPage extends React.Component <{},{name : string, username:str
     this.handleSignUpConfirmPassword = this.handleSignUpConfirmPassword.bind(this);
     this.handleSignUpEmail = this.handleSignUpEmail.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.headers = new Headers({ 'Content-Type': 'application/json', 'Accept': 'q=0.8;application/json;q=0.9' });
+    }
+
+    postData(){
+        var cats: any;
+        var ceva = {id:0, username: this.state.username, password: this.state.password, name: this.state.name, email: this.state.email}
+        var form2 = JSON.stringify(ceva);
+        console.log(form2);
+        cats = '';
+        return fetch(this.baseUrl, {method : "POST", body: form2, headers:this.headers})
+            .then((response) => response.json())
+            .then(function (data) {
+                cats = data;
+                console.log(cats);
+            })
+            .catch(function (error) {
+                console.log('request failedddd', error)
+            })
     }
 
     handleSignUpName(event : React.FormEvent<HTMLInputElement>){
@@ -28,8 +49,9 @@ export class SignUpPage extends React.Component <{},{name : string, username:str
     handleSignUpEmail(event : React.FormEvent<HTMLInputElement>){
             this.setState({email:event.currentTarget.value});
     }
-    handleSubmit(event : React.FormEvent<HTMLInputElement>){
-
+    handleSubmit(e :any){
+        e.preventDefault();
+        this.postData();
     }
 
     render(){
@@ -81,7 +103,7 @@ export class SignUpPage extends React.Component <{},{name : string, username:str
                             </span>
                         </div>
                         <div className="spacing">
-                            <input type='submit' className="btn btn-primary" value="Sign Up"/>
+                            <button className="btn btn-primary" onClick={this.handleSubmit}>Sign Up</button>
                         </div>
                     </form>
                 </div>
