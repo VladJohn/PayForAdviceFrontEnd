d363a34ccef9be19ec700bbb8b8e73c860c35625
WIRTEK\luiza.tanasie <luiza.tanasie197@gmail.com>
2017-08-02 11:33:44 +0300

    main page design

--- app/Components/Elements/Category.tsx
+++ app/Components/Elements/Category.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-export class Category extends React.Component<{type : string, imgurl: string, name: string, description : string, id : number},{}>
+export class Category extends React.Component<{type : string, imgurl: string, name: string, description : string, id : number, iconurl: string},{}>
 {
 
   render(){
@@ -9,9 +9,12 @@ export class Category extends React.Component<{type : string, imgurl: string, na
             <div className="col-md-4">
                 <img className="listimage img-responsive" src={this.props.imgurl}></img>
             </div>
-            <div className="col-md-5">
-               <div><h3> {this.props.name}</h3> </div>
+            <div className="col-md-7">
+               <div className="category-title"><h3> {this.props.name}</h3> </div>
                <div> {this.props.description} </div>
+            </div>
+            <div className="col-md-1">
+               <img className="listimage img-responsive" src={this.props.iconurl}></img>
             </div>
         </div>
     );
